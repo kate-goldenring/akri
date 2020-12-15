9a66aeb6090fdf902f42c640e99c9af6f37ce00b
Kate Goldenring <kate.goldenring@microsoft.com>
2020-12-14 20:09:10 -0800

    update onvif script with correct network

--- workshop/vm-scripts/start-onvif-camera.sh
+++ workshop/vm-scripts/start-onvif-camera.sh
@@ -18,6 +18,6 @@ fi
 if pgrep onvif_srvd > /dev/null; then
     sudo pkill -9 onvif_srvd
 fi
-ip4=$(/sbin/ip -o -4 addr list eno1 | awk '{print $4}' | cut -d/ -f1)
-sudo ./onvif_srvd/onvif_srvd  --ifs eno1 --scope onvif://www.onvif.org/name/TestDev --scope onvif://www.onvif.org/Profile/S --name RTSP --width 800 --height 600 --url rtsp://$ip4:8554/stream1 --type MPEG4
-./wsdd/wsdd  --if_name eno1 --type tdn:NetworkVideoTransmitter --xaddr http://%s:1000/onvif/device_service --scope "onvif://www.onvif.org/name/Unknown onvif://www.onvif.org/Profile/Streaming"
+ip4=$(/sbin/ip -o -4 addr list etho0 | awk '{print $4}' | cut -d/ -f1)
+sudo ./onvif_srvd/onvif_srvd  --ifs etho0 --scope onvif://www.onvif.org/name/TestDev --scope onvif://www.onvif.org/Profile/S --name RTSP --width 800 --height 600 --url rtsp://$ip4:8554/stream1 --type MPEG4
+./wsdd/wsdd  --if_name etho0 --type tdn:NetworkVideoTransmitter --xaddr http://%s:1000/onvif/device_service --scope "onvif://www.onvif.org/name/Unknown onvif://www.onvif.org/Profile/Streaming"
