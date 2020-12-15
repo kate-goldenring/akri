d00407a277c7dd79179dc18eccfdbad189c9fe38
Kate Goldenring <kate.goldenring@microsoft.com>
2020-12-15 14:34:08 -0800

    fix etho to eth

--- workshop/vm-scripts/start-onvif-camera.sh
+++ workshop/vm-scripts/start-onvif-camera.sh
@@ -18,6 +18,6 @@ fi
 if pgrep onvif_srvd > /dev/null; then
     sudo pkill -9 onvif_srvd
 fi
-ip4=$(/sbin/ip -o -4 addr list etho0 | awk '{print $4}' | cut -d/ -f1)
-sudo ./onvif_srvd/onvif_srvd  --ifs etho0 --scope onvif://www.onvif.org/name/TestDev --scope onvif://www.onvif.org/Profile/S --name RTSP --width 800 --height 600 --url rtsp://$ip4:8554/stream1 --type MPEG4
-./wsdd/wsdd  --if_name etho0 --type tdn:NetworkVideoTransmitter --xaddr http://%s:1000/onvif/device_service --scope "onvif://www.onvif.org/name/Unknown onvif://www.onvif.org/Profile/Streaming"
+ip4=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
+sudo ./onvif_srvd/onvif_srvd  --ifs eth0 --scope onvif://www.onvif.org/name/TestDev --scope onvif://www.onvif.org/Profile/S --name RTSP --width 800 --height 600 --url rtsp://$ip4:8554/stream1 --type MPEG4
+./wsdd/wsdd  --if_name eth0 --type tdn:NetworkVideoTransmitter --xaddr http://%s:1000/onvif/device_service --scope "onvif://www.onvif.org/name/Unknown onvif://www.onvif.org/Profile/Streaming"
